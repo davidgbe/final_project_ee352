e25a91e33a21bbb153cc97b25846e44ac89ffa59
CalvinLeGassick <calvin.legassick@gmail.com>
2015-05-07 00:24:36 -0700

    cleaned up output

--- david.asm
+++ david.asm
@@ -4,10 +4,17 @@
 	ask_for_lines_per_set: .asciiz "Please provide the number of lines per set: "
 	ask_for_cache_size: .asciiz "Please provide a size for the cache (in KB and a multiple of the set size): "
 	invalid_cache_size: .asciiz "Cache size was not divisible by set size"
-	test_message: .asciiz "TEST: "
+	number_of_accesses_message: .asciiz "Number of accesses in input file: "
+	number_of_sets_text: .asciiz "Number of sets: "
+	set_size_text: .asciiz "Size of each set: "
+	number_of_lines_text: .asciiz "Number of lines: " 
+	size_of_tag_text: .asciiz "Size of tag: " 
+	bits_needed_offset_text: .asciiz "Bits needed for offset: "
+	bits_needed_sets_text: .asciiz "Bits needed for sets: "
 	
 	#helpful for printing
 	newline: .asciiz "\n"
+	test_message: .asciiz "TEST: "
 	
 	#data
 	main_memory: .word 0 : 65536 #roughly 65KB of storage in main memory
@@ -27,11 +34,73 @@
 	access_queue: .word 0
 	access_queue_size: .word 200
 	
+	#input data
+	buffer: .space 1400
+	trace_file: 		.asciiz "trace.txt"
+	input_error_string:	.ascii  "Error on input \n"
+	trace_file_line_length: .word 7
+	
 	test: .word 536870397
 	
 .text
 
 main:
+
+open_file:
+	li	$v0, 13		# Open File Syscall
+	la	$a0, trace_file	# Load File Name
+	li	$a1, 0		# Read-only Flag
+	li	$a2, 0		# (ignored)
+	syscall
+	move	$s6, $v0	# Save File Descriptor
+	blt	$v0, 0, err	# Goto Error
+ 
+# Read Data
+read:
+	li	$v0, 14		# Read File Syscall
+	move	$a0, $s6	# Load File Descriptor
+	la	$a1, buffer	# Load Buffer Address
+	li	$a2, 1400	# Buffer Size
+	syscall
+	
+save_number_of_accesses:
+
+	move $t0, $v0	#save size of file
+	
+	#get size of access queue (divide size by file_line_length and add 1, due to trace.txt file format)
+	lw $t1, trace_file_line_length($zero)
+	div $t0, $t1
+	mflo $t1
+	addi $t1, $t1, 1
+	sw $t1, access_queue_size
+	
+	li $v0, 4
+	la $a0, number_of_accesses_message
+	syscall
+	
+	li $v0 1
+	move $a0, $t1
+	syscall
+	
+	li $v0, 4
+	la $a0, newline
+	syscall
+	
+	li $t1, 2	#starting point of important info from first address (sets up for loop)
+	
+loop_through_lines:
+	
+	jal get_one_line
+	addi $t1, $t1, 3				#skip to beginning of address on next line
+	blt $t1, $t0, loop_through_lines		#keep iterating until done
+	
+# Close File
+close_file:
+	li	$v0, 16		# Close File Syscall
+	move	$a0, $s6	# Load File Descriptor
+	syscall
+
+begin_collection:
 	#collect input
 	la $t7, ask_for_line_size 
 	jal collectInput
@@ -49,13 +118,18 @@ main:
 	mult $s1, $s2
 	mflo $s3
 	sw $s3, set_size
-	move $t7, $s3
-	jal printInt #print set size
+
+	#move $t7, $s3
+	#jal printInt #print set size
 	
 	addi $t0, $s1, 5
 	mult $s2, $t0
 	mflo $t0
 	sw $t0, actual_set_size
+	
+	li $v0, 4
+	la $a0, set_size_text
+	syscall
 	move $t7, $t0
 	jal printInt #print actual set size
 	
@@ -71,6 +145,9 @@ main:
 	mflo $s4 #number of lines
 	sw $s4, num_lines
 	
+	li $v0, 4
+	la $a0, number_of_lines_text
+	syscall
 	move $t7, $s4
 	jal printInt #print number of lines
 	
@@ -78,6 +155,10 @@ main:
 	mfhi $t1 #cache size not divisible by set size
 	mflo $s5 #number of sets
 	sw $s5, num_sets
+	
+	li $v0, 4
+	la $a0, number_of_sets_text
+	syscall
 	move $t7, $s5
 	jal printInt #print number of sets
 	
@@ -94,8 +175,10 @@ allocation:
 	mflo $t1
 	add $s0, $s0, $t1 #add an extra 5 bytes for each line for valid bit and tag
 	sw $s0, actual_cache_size
-	move $t7, $s0
-	jal printInt #print size of cache with extra size for info
+	
+	#move $t7, $s0
+	#jal printInt #print size of cache with extra size for info
+	
 	#dynamic allocation of cache
 	li $v0, 9
 	move $a0, $s0 #where $s0 has number of bytes to allocate
@@ -108,6 +191,9 @@ allocation:
 	sw $t8, set_index_size
 	move $s7, $t8
 	
+	li $v0, 4
+	la $a0, bits_needed_sets_text
+	syscall
 	move $t7, $t8
 	jal printInt
 	
@@ -116,6 +202,9 @@ allocation:
 	sw $t8, offset_size
 	move $a3, $t8
 	
+	li $v0, 4
+	la $a0, bits_needed_offset_text
+	syscall
 	move $t7, $t8
 	jal printInt
 	
@@ -123,19 +212,12 @@ allocation:
 	sub $s6, $s6, $s7
 	sub $s6, $s6, $a3 
 	sw $s6, tag_size
+	
+	li $v0, 4
+	la $a0, size_of_tag_text
+	syscall
 	move $t7, $s6
 	jal printInt #print tag size
-	
-	lw $s0, test #print calculate set address
-	move $t8, $s0
-	jal checkCache
-	move $t0, $t7
-	move $t7, $t6
-	jal printInt
-	move $t7, $t0
-	jal printInt
-	move $t7, $t8
-	jal printInt
 	
 end:
 	#end of program
@@ -144,7 +226,6 @@ end:
 	
 	
 	
-
 checkCache: #expects address at $t8. Returns hit/miss in $t6, line address in $t7, and data $t8
 	move $s4, $ra
 	move $s0, $t8 #move the address
@@ -307,3 +388,102 @@ leftShiftLoop:
 	j leftShiftLoop
 leftShiftDone:
 	jr $ra
+	
+get_one_line: #expects "index" value in t1
+ 	sub $sp,$sp,4 # push ra
+	sw $ra,4($sp)
+	
+	add $t2, $zero, $zero	#reset
+	
+	#converts a row of 4 ascii char in hex format, and stores them in $t5
+	
+	lbu $t3, buffer($t1)	#MSBB "bucket"
+	jal hexchar_to_int
+	addi $t6, $zero, 4096	
+	mult $t3, $t6		#16 Cubed
+	mflo $t3
+	add $t2, $t2, $t3
+	
+	addi $t1, $t1, 1
+	
+	lbu $t3, buffer($t1)	#next "bucket"
+	jal hexchar_to_int
+	addi $t6, $zero, 256
+	mult $t3, $t6		#16 Squared
+	mflo $t3
+	add $t2, $t2, $t3
+	
+	addi $t1, $t1, 1
+	
+	lbu $t3, buffer($t1)	#next "bucket"
+	jal hexchar_to_int
+	addi $t6, $zero, 16
+	mult $t3, $t6		#16
+	mflo $t3
+	add $t2, $t2, $t3
+	
+	addi $t1, $t1, 1
+	
+	lbu $t3, buffer($t1)	#LSB "bucket"
+	jal hexchar_to_int
+	addi $t6, $zero, 1
+	mult $t3, $t6		#1
+	mflo $t3
+	add $t2, $t2, $t3
+	
+	addi $t1, $t1, 1
+	
+#	li $v0, 1
+#	move $a0, $t2
+#	syscall
+#	
+#	li $v0, 4
+#	la $a0, newline
+#	syscall
+	
+	#store converted address in proper location
+	lw $t4, trace_file_line_length($zero)
+	div $t1, $t4
+	mflo $t4
+	sll $t4, $t4, 2
+	
+	sw $t2, access_queue($t4)
+	
+	lw $ra,4($sp) # pop ra
+	add $sp,$sp,4
+	jr $ra
+	
+hexchar_to_int: #expect arg in $t3
+
+        li $t8,0X30
+        li $t9,0x39
+
+        andi    $t8,$t8,0x000000ff #Cast to word for comparison.
+        andi    $t9,$t9,0x000000ff
+
+        bltu    $t3,$t8,err     #error if lower than 0x30
+        bgt     $t3,$t9,dohex     #if greater than 0x39, test for A -F
+
+        addiu   $t3,$t3,-0x30     #OK, char between 48 and 55. Subtract 48.
+        jr $ra
+
+dohex:  li      $t8,0x41
+        li      $t9,0x46
+
+        andi   $t8,$t8,0x000000ff #Cast to word for comparison.
+        andi   $t9,$t9,0x000000ff
+
+        #is byte is between 65 and 70
+
+        bltu    $t3,$t8,err     #error if lower than 0x41
+        bgt     $t3,$t9,err     #error if greater than 0x46
+
+ishex:  addiu   $t3,$t3,-0x37     #subtract 55 from hex char ('A'- 'F')
+        jr $ra
+	
+# Error
+err:
+	li	$v0, 4			# Print String Syscall
+	la	$a0, input_error_string	# Load Error String
+	syscall
+	b end
